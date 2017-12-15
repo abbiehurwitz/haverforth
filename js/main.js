6784fa9b18a35c68474785da8859d6f8a68d13da
abbiehurwitz <34584513+abbiehurwitz@users.noreply.github.com>
2017-12-15 16:38:16 -0500

    Update main.js
    Hopefully this is the right way to commit this. Just in case I have also made my own repository with the code and the reflection which I will make you a contributor to so you can access it.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,100 @@
 // See the following on using objects as key/value dictionaries
 // https://stackoverflow.com/questions/1208222/how-to-do-associative-array-hashing-in-javascript
-var words = {};
 
-/** 
+/**
  * Your thoughtful comment here.
  */
 function emptyStack(stack) {
     // ...
 }
+function emptyStack(stack){
+  while(stack.length > 0){
+    stack.pop();
+  }
+}
+function add(stack){
+  var first = stack.pop();
+  var second = stack.pop();
+  stack.push(first+second);
+}
+function subtract(stack){
+  var first = stack.pop();
+  var second = stack.pop();
+  stack.push(first-second);
+}
+function multiply(stack){
+  var first = stack.pop();
+  var second = stack.pop();
+  stack.push(first*second);
+}
+function divide(stack){
+  var first = stack.pop();
+  var second = stack.pop();
+  stack.push(first/second);
+}
+function nip(stack){
+  stack.pop()
+}
+function swap(stack){
+  var first = stack.pop()
+  var second = stack.pop()
+  stack.push(first);
+  stack.push(second);
+}
+function over(stack){
+  var first = stack.pop()
+  var second = stack.pop()
+  stack.push(first);
+  stack.push(second);
+  stack.push(first);
+}
+function greaterThan(stack){
+  var first = stack.pop()
+  var second = stack.pop()
+  if(first > second){
+    stack.push(0);
+  }
+  else{
+    stack.push(-1);
+  }
+}
+function lessThan(stack){
+  var first = stack.pop();
+  var second = stack.pop();
+  if(first < second){
+    stack.push(0);
+  }
+  else{
+    stack.push(-1);
+  }
+}
+function equalTo(stack){
+  var first = stack.pop()
+  var second = stack.pop()
+  if(first == second){
+    stack.push(0);
+  }
+  else{
+    stack.push(-1);
+  }
+}
+function printS(stack){
+  print(terminal, " <" + stack.length + "> " + stack.slice().join(" "));
+}
+var builtIn = {};
+builtIn["+"] = add;
+builtIn["-"] = subtract;
+builtIn["*"] = multiply;
+builtIn["/"] = divide;
+builtIn["nip"] = nip;
+builtIn["swap"] = swap;
+builtIn["over"] = over;
+builtIn["<"] = greaterThan;
+builtIn[">"] = lessThan;
+builtIn["="] = equalTo;
+builtIn[".s"] = printS
+
+var userDefined = {};
 
 /**
  * Print a string out to the terminal, and update its scroll to the
@@ -21,7 +108,7 @@ function print(terminal, msg) {
     $("#terminal").scrollTop($('#terminal')[0].scrollHeight + 40);
 }
 
-/** 
+/**
  * Sync up the HTML with the stack in memory
  * @param {Array[Number]} The stack to render
  */
@@ -32,29 +119,58 @@ function renderStack(stack) {
     });
 };
 
-/** 
+/**
  * Process a user input, update the stack accordingly, write a
  * response out to some terminal.
  * @param {Array[Number]} stack - The stack to work on
  * @param {string} input - The string the user typed
  * @param {Terminal} terminal - The terminal object
  */
-function process(stack, input, terminal) {
-    // The user typed a number
-    if (!(isNaN(Number(input)))) {
-        print(terminal,"pushing " + Number(input));
-        stack.push(Number(input));
-    } else if (input === ".s") {
-        print(terminal, " <" + stack.length + "> " + stack.slice().join(" "));
-    } else if (input === "+") {
-        var first = stack.pop();
-        var second = stack.pop();
-        stack.push(first+second);
-    } else {
-        print(terminal, ":-( Unrecognized input");
-    }
-    renderStack(stack);
-};
+ function uprocess(stack, key, terminal){
+   var str = userwords[key];
+   process(stack, str, terminal);
+ }
+
+var userfunction = [];
+
+   
+ function process(stack, input, terminal) {
+   var inputs = input.trim().split(/ +/);
+     var startFunc = false;
+     var getName = false;
+     for(var c = 0; c < inputs.length; c++) {
+       //if number push onto stack
+       if (!(isNaN(Number(inputs[c]))) && startFunc === false) {
+           stack.push(Number(inputs[c]));
+       } else if (inputs[c] == ":"){
+         //start of user defined function
+           var getName = true;
+           var Username;
+           //var userfunc = [];
+       } else if (getName === true){
+           Username = inputs[c];
+           getName = false;
+           startFunc = true;
+       } else if (startFunc === true && inputs[c] != ";") {
+         //create list of user function
+           userfunc.push(inputs[c]);
+       } else if (inputs[c] == ";"){
+           var string = userfunc.join(" ");
+           userDefined[name] = string;
+           startFunc = false;
+       } else if (builtIn.hasOwnProperty(inputs[c]) === true && startFunc === false) {
+         //checks if built in function and runs
+           words[inputs[c]](stack);
+       } else if (userDefined.hasOwnProperty(inputs[c]) === true && startFunc === false) {
+         //checks if user defined function and runs
+           (stack, inputs[c], terminal);
+       } else {
+           print(terminal, ":-( Unrecognized input");
+       }
+     }
+     renderStack(stack);
+ };
+
 
 function runRepl(terminal, stack) {
     terminal.input("Type a forth command:", function(line) {
@@ -64,21 +180,25 @@ function runRepl(terminal, stack) {
     });
 };
 
-// Whenever the page is finished loading, call this function. 
+
+// Whenever the page is finished loading, call this function.
 // See: https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function() {
     var terminal = new Terminal();
     terminal.setHeight("400px");
     terminal.blinkingCursor(true);
-    
+
     // Find the "terminal" object and change it to add the HTML that
     // represents the terminal to the end of it.
     $("#terminal").append(terminal.html);
 
     var stack = [];
-
+    var resetbutton = $("#reset");
+    renderStack(stack);
+    $("#reset").click(function() { while(stack.length > 0) { stack.pop(); } });
     print(terminal, "Welcome to HaverForth! v0.1");
     print(terminal, "As you type, the stack (on the right) will be kept in sync");
 
     runRepl(terminal, stack);
 });
+
